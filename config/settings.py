24c4a27c7207b699fdb894168cac7c6360d1331c
TMCreme <tonnybright123@yahoo.com>
2024-03-05 23:55:37 +0000

    storages fixed on django storages

--- config/settings.py
+++ config/settings.py
@@ -180,11 +180,11 @@ if USE_S3:
     # s3 static settings
     STATIC_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+    STATICFILES_STORAGE = "storages.backends.s3.S3Storage"
     # s3 public media settings
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = "storages.backends.s3.S3Storage"
 else:
     STATIC_URL = 'static/'
     STATIC_ROOT = os.environ.get("STATIC_DIR")
